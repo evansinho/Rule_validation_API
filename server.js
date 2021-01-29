2f18b1e0ad07ea4fa2effabfb846919884aa8811
evanson <igiri.evanson@gmail.com>
2021-01-29 16:23:27 +0100

    setup mongo db using mongoose

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Instantiate the app
 const app = express();
 // Define our app port.
 const port = process.env.PORT || 3000;
+
 // db config
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
 
 // middlewares
 
